c36d6f4c4f25bb7ae4ddda2af518d4ce173d2438
Haruki-Kawazoe <75521368+Haruki-Kawazoe@users.noreply.github.com>
2022-05-27 18:14:18 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,119 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    control.reset()
+})
+input.onButtonPressed(Button.A, function () {
+    led.enable(false)
+    while (0 == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        basic.pause(25000)
+        for (let index = 0; index < 8; index++) {
+            pins.digitalWritePin(DigitalPin.P3, 1)
+            basic.pause(250)
+            pins.digitalWritePin(DigitalPin.P3, 0)
+            basic.pause(250)
+        }
+        pins.digitalWritePin(DigitalPin.P4, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(3000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(30000)
+    }
+})
+basic.showString("Hello!")
+while (0 == 0) {
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . . # . .
+        . . . . .
+        . # # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        . # # # .
+        . . # . .
+        . . . . .
+        . # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . # . .
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # # .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(500)
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
+    basic.pause(500)
+}
